0a192b54d7f4fb672df66a7523deea36aa4990a5
Fima <FimaRozovsky@hotmail.com>
2017-06-07 16:47:23 +0300

    added search functionality + keywords for possible search

--- js/main.js
+++ js/main.js
@@ -2,35 +2,77 @@
 console.log("main launched!");
 var gElScreen ={elPickMeme:null, elGenMeme:null};
 var gElInput = {bottomText:null, topText:null};
-//var gTextDrawn = {top:false, bottom:false};
+
+var gImgs =[{ url:'assets/img/memeGen/0.png', keywords:[ 'presice', 'determined' ] },
+            { url:'assets/img/memeGen/1.png', keywords:[ 'doubtful', 'not sure' ] },
+            { url:'assets/img/memeGen/2.png', keywords:[ 'humiliated', 'angry' ] },
+            { url:'assets/img/memeGen/3.png', keywords:[ 'happy', 'stoned' ] },
+            { url:'assets/img/memeGen/4.png', keywords:[ 'hopefull', 'scared' ] },
+            { url:'assets/img/memeGen/5.png', keywords:[ 'dead', 'patient' ]},
+            { url:'assets/img/memeGen/6.png', keywords:[ 'interested', 'determined' ] },
+            { url:'assets/img/memeGen/7.png', keywords:[ 'happy', 'inviting' ] }
+            ];
+
 
 var gIsRecursing = false;
 
-var elSavedImg;
+var gElSavedImg;
 var gElMemeCanvas;
+var gElSearchInput;
 
 function init(){
-    gElScreen.elPickMeme = document.querySelector('#screen-pick-meme');
-    gElScreen.elGenMeme = document.querySelector('#screen-meme-gen');
-    gElMemeCanvas = document.querySelector('#canvas-meme-gen');
+    gElScreen.elPickMeme = document.querySelector( '#screen-pick-meme' );
+    gElScreen.elGenMeme = document.querySelector( '#screen-meme-gen' );
+    gElMemeCanvas = document.querySelector( '#canvas-meme-gen' );
 
-    gElInput.topText = document.querySelector('[name="top-text"]');
-    gElInput.bottomText = document.querySelector('[name="bottom-text"]');
+    gElInput.topText = document.querySelector( '[name="top-text"]' );
+    gElInput.bottomText = document.querySelector( '[name="bottom-text"]' );
+
+    gElSearchInput = document.querySelector( '[name="search-text"]' );
 
     gElInput.topText.oninput = addTopText;
     gElInput.bottomText.oninput = addBottomText;
+    gElSearchInput.oninput = search;
+    renderMemeTable(gImgs);
+    renderSearchArray();
 }   
-function pickAMeme(elImg){
-    elSavedImg = elImg;
-    gElScreen.elPickMeme.classList.toggle('screen-inactive');
-    gElScreen.elPickMeme.classList.toggle('screen-active');
+function renderMemeTable(imgs){
+    var htmlCode = "";
+    imgs.forEach( function(img){
+        htmlCode += `<div class="image"><img src="${img.url}" onclick="changeAScreen(this)"></div>`
+    });
+    var elImages = document.querySelector('.images');
+    elImages.innerHTML = htmlCode;
 
-    gElScreen.elGenMeme.classList.toggle('screen-inactive');
+}
+function renderSearchArray(){
+    var htmlCode = '';
+    var keywords =[];
+    gImgs.forEach( function(img){
+        img.keywords.forEach( function(keyword){
+            htmlCode += `<div>${keyword}</div>`;
+            keywords.push(keyword);
+        });
+    });
+    var elSearchArray = document.querySelector('.search-array');
+    elSearchArray.innerHTML = htmlCode;
+}
+function changeAScreen(elImg){
+    gElSavedImg = elImg;
+
     gElScreen.elGenMeme.classList.toggle('screen-active');
+    
+    gElScreen.elPickMeme.classList.toggle('screen-inactive');
+    gElScreen.elPickMeme.classList.toggle('screen-active');  
+    gElScreen.elGenMeme.classList.toggle('screen-inactive');   
+
+   
     drawAMeme(elImg);
 }
 function drawAMeme(elImg){ 
     var ctx = gElMemeCanvas.getContext("2d");
+    if(!elImg)  return;
+    ctx.clearRect(0, 0, gElMemeCanvas.width, gElMemeCanvas.height);
     ctx.drawImage(elImg,(gElMemeCanvas.width - elImg.width) / 2,(gElMemeCanvas.height - elImg.height) /2);
 }
 
@@ -60,15 +102,29 @@ function drawText(canvas, text, width,height){
 
     ctx.fillText(text,width, height);
     ctx.strokeText(text,width, height);
-
 }
 function drawMemeWithText(){
-    drawAMeme(elSavedImg);
+    drawAMeme(gElSavedImg);
     addTopText();
     addBottomText();
+
+}
+function search(){
+    var matchingImgs = gImgs.filter( function(img){
+        var returnValue = false;
+        img.keywords.forEach(function(keyword){
+            if(keyword.search(gElSearchInput.value) !== -1){
+                returnValue = true;
+            }
+            
+        });
+        return returnValue;  
+    });
+    renderMemeTable(matchingImgs);
 
 }
 window.onbeforeunload = function(e) {
     gElInput.bottomText.value = null;
     gElInput.topText.value = null;
+    gElSearchInput.value = null;
 }
